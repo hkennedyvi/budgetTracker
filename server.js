12f63caf9091733f71e003ef34ac0c3f37ae24d9
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-08 14:43:04 -0700

    mongoDB connection edit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds249035.mlab.com:49035/heroku_bwf6r6cb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
